3464e3e6f970e444093c345c22e4f5355a9006c0
Rajasah2000 <rajasah30030@gmail.com>
2024-09-26 19:45:03 +0530

    up

---
+++ src/Pages/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../Pages/Login.css"
+const Login = () => {
+  return (
+    <div className='box'>
+      <form>
+        <h2>Sign in</h2>
+        <div className='inputBox'>
+            <input type='text'/>
+            <span>User name</span>
+            <i></i>
+        </div>
+        <div className='inputBox'>
+            <input type='password'/>
+            <span>password</span>
+            <i></i>
+        </div>
+        <div className='links'>
+            <a href='#'>Forgot password</a>
+            <a href='#'> Signup</a>
+        </div>
+        <input type='submit' value="Login"/>
+      </form>
+    </div>
+  )
+}
+
+export default Login
